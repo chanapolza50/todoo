3535f2c3b20bc8be21ce1398239332bc1d19d6a8
chanapol reansaokean <chanapol_ru56@live.rmutl.ac.th>
2017-07-07 11:13:48 +0700

    Initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new \App\User();
+        $user->name = "chanapol ruensakean";
+        $user->user_type_id = "1";
+        $user->username = "admin";
+        $user->email = "chanapol50@hotmail.com";
+        $user->password = bcrypt('1234');
+        $user->save();
+    }
+}
